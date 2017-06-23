186f2a24853a4ba6a96c2c90987d3dc5901151a6
hidoge <308680@gmail.com>
2017-06-23 14:32:48 +0800

    async components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Tables from '@/components/tables'
 
 Vue.use(Router)
 
@@ -10,12 +8,16 @@ export default new Router({
     {
       path: '/',
       name: 'Tables',
-      component: Tables,
+      component(resolve) {
+        require(['@/components/Hello'], resolve)
+      },
     },
     {
       path: '/hello',
       name: 'Hello',
-      component: Hello,
+      component(resolve) {
+        require(['@/components/tables'], resolve)
+      },
     },
   ],
 })
