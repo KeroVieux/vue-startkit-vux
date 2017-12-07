8362be1f04ac7e068e449054181d78623bd1a742
hidoge <308680@gmail.com>
2017-12-07 16:45:55 +0800

    add await-to-js

--- src/assets/js/apis-mixins.js
+++ src/assets/js/apis-mixins.js
@@ -3,6 +3,7 @@
  */
 
 import queryString from 'query-string'
+import To from 'await-to-js'
 
 /**
  * This provides mixins used for common api in the Vue components.
@@ -13,19 +14,19 @@ import queryString from 'query-string'
  */
 
 const ApiMixin = {
-  async apiComments(arg = { method: 'get', params: {} }) {
-    let res = null
+  async apiArticles(arg = { method: 'get', params: {} }) {
+    let [err, req] = [null, null]
     switch (arg.method) {
       case 'post':
-        res = await axios.post(`${currentEnv.diningServer}/api/comments`, arg.params)
+        [err, req] = await To(axios.post('articles/', arg.params))
         break
       case 'patch':
-        res = await axios.patch(`${currentEnv.diningServer}/api/messages/${arg.params.related_message_id}/comments/${arg.params.comment_id}`, arg.params)
+        [err, req] = await To(axios.patch(`articles/${arg.params.article_id}`, arg.params))
         break
       default:
-        res = await axios.get(`${currentEnv.diningServer}/api/messages/${arg.params && arg.params.message_id ? arg.params.message_id : ''}?${queryString.stringify(arg.params)}`)
+        [err, req] = await To(axios.get(`articles/${arg.params && arg.params.article_id ? arg.params.article_id : ''}?${queryString.stringify(arg.params)}`))
     }
-    return res.data
+    return this.globalCB(err, req)
   },
   /**
    * Generate the logs on server , when the user have done something sensitive with server requires,
@@ -38,16 +39,6 @@ const ApiMixin = {
     return res.data
   },
   /**
-   * Get information of ldap user from server,
-   * ordinary for fetch a specific user's info
-   * @param {object} arg depId: 'CR0032000059', oprId: 'ANXING'
-   * @returns {object} object of user's info
-   */
-  async apiLdapUsers(arg) {
-    const req = await axios.get(`${currentEnv.wxServer}/api/qy-wexin/ldap-users?${queryString.stringify(arg)}`)
-    return req.data
-  },
-  /**
    * Get information of wx user from server,
    * ordinary for detect a user's auth when it entered app at once
    * @param {object} params agentId, code, fake
@@ -57,12 +48,39 @@ const ApiMixin = {
     const req = await axios.get(`${currentEnv.wxServer}/api/qy-wexin/user_detail?${queryString.stringify(params)}`)
     return req.data
   },
+  /**
+   * Get information of ldap user from server,
+   * ordinary for fetch a specific user's info
+   * @param {object} arg depId: 'CR0032000059', oprId: 'ANXING'
+   * @returns {object} object of user's info
+   */
+  async ldapUsers(arg) {
+    const [err, req] = await To(axios.get(`${currentEnv.wxServer}/api/qy-wexin/ldap-users?${queryString.stringify(arg)}`))
+    return this.globalCB(err, req)
+  },
+  globalCB(err, req) {
+    if (err) {
+      this.$vux.toast.show({
+        type: 'text',
+        text: _.toString(err),
+      })
+      return { error: _.toString(err) }
+    } else if (req && req.data && req.data.status === 401) {
+      this.$vux.toast.show({
+        type: 'text',
+        text: '请先登录',
+      })
+      this.$router.push('/entrances/signin')
+    } else if (req && req.data && req.data.error) {
+      this.$vux.toast.show({
+        type: 'text',
+        text: _.toString(req.data.error),
+      })
+    }
+    return req.data
+  },
 }
 
 export default {
-  created() {
-    document.documentElement.scrollTop = 0
-    document.body.scrollTop = 0
-  },
   methods: ApiMixin,
 }
